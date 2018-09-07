8d12ca7504c1588198d063e6fa882458d459be07
mixnam <mixnam0905@gmail.com>
2018-09-07 19:33:43 +0300

    add angular

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
-function makeRequest(method, url){
-	return new Promise(function(resolved,rejected){
-		var xhr = new XMLHttpRequest();
-		xhr.open(method, url);
-		xhr.onload = function(){
-			if (this.status == 200){
-				resolved(xhr.response)
-			}else{
-				rejected({
-					status: this.status,
-					stetusText: xhr.statusText
-				})
-			}
-		}
-		xhr.onerror = function(){
-			rejected({
-				status: this.status,
-				statusText: xhr.statusText
-			})
-		}
-		xhr.send()
-	})
-}
+var app = angular.module('TestApp',[])
 
-function getCurrencyCoef(baseCurrency, convertCurrency){ 
-	 return makeRequest('GET',
-						`http://free.currencyconverterapi.com/api/v5/convert?\
-						q=${baseCurrency}_${convertCurrency}&compact=y`).then(
-							(resp)=>{
-								let value = JSON.parse(resp)
-								return value[baseCurrency + '_' + convertCurrency].val
-							}
-						).catch(
-							(resp)=>{
-								return resp
-							}
-						)
-}
+app.controller('TestAppController',function($scope,$http){
+  var self = this;
+  
+  var currencyList = ['EUR',  //евро
+                      'RUB', //рубль
+                      'GBP', //фунт
+                      'CNY', //китайская йена
+                      'USD'] //доллар
+  
+  self.currency = 'USD'
+  self.selectedCart = [
+    {price: 20},
+    {price: 45},
+    {price: 67},
+    {price: 1307}
+  ]
+  
+  self.getCurrentTotalCartPrice = function(){
+    return self.selectedCart.reduce(function(accum, item){
+      return isNaN(Number(item.price))?accum:accum + Number(item.price)},0)
+  }
+  
+  self.totalCartPrice = {}
+  
+  self.recalcTotal = function(){
+    var currentTotalCartPrice = self.getCurrentTotalCartPrice()
+    
+    currencyList.forEach((newCurrency)=>{
+       var url = `https://free.currencyconverterapi.com/api/v6/convert?q=${self.currency}_${newCurrency}&compact=y`
+       
+       $http.get(url,{cache: true}).then((data,status)=>{
+         let key = self.currency + '_' + newCurrency
+         let coef = data.data[key].val
+         self.totalCartPrice[newCurrency] = (coef * currentTotalCartPrice).toFixed(2)
+       }).catch((data,status)=>{
+         console.log(data);
+         console.log(status)
+       })
+    })
+  }
+})
